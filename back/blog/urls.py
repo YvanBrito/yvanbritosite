ebbd85abb9ada1af8ad04e8bca97a53d4e525da0
Yvan Brito <yvanbrito96@gmail.com>
2020-05-22 15:40:51 -0300

    Atualizando resume

--- back/blog/urls.py
+++ back/blog/urls.py
@@ -11,10 +11,10 @@ from . import views
 
 urlpatterns = [
     path('blog', PostListView.as_view(), name='blog-home'),
-    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('post/new/', PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
-    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
-    path('about/', views.about, name='blog-about'),
+    path('blog/user/<str:username>', UserPostListView.as_view(), name='user-posts'),
+    path('blog/post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('blog/post/new/', PostCreateView.as_view(), name='post-create'),
+    path('blog/post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+    path('blog/post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
+    path('blog/about/', views.about, name='blog-about'),
 ]
