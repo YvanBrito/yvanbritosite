a7439f2b4e264ebd3daf9d0a95ae2f7a07376c07
Yvan Brito <yvanbrito96@gmail.com>
2020-05-24 22:47:44 -0300

    Update settings.py

--- back/web_blog/settings.py
+++ back/web_blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ks77d4ge*@-j$f4hr@r*&33ib+n7$2jfaeopm601_gfvvwndzm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
